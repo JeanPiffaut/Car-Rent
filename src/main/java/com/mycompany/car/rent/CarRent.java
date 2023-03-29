d7eea41cbc654a868942d556511547075b8da943
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2023-03-29 20:48:39 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/car/rent/CarRent.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt 
+ * to change this license
+ */
+
+package com.mycompany.car.rent;
+
+/**
+ *
+ * @author piffa
+ */
+public class CarRent {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/car/rent/Vehiculo.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.car.rent;
+
+import javax.naming.directory.InvalidAttributesException;
+
+/**
+ *
+ */
+public final class Vehiculo {
+    private String patente;
+    private String marca;
+    private String modelo;
+    private int anho;
+    private String condicion;
+
+    public Vehiculo(String patente, String marca, String modelo, int anho) {
+        setPatente(patente);
+        setMarca(marca);
+        setModelo(modelo);
+        setAnho(anho);
+        setCondicion("D");
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        if (patente.length() != 8) {
+            throw new IllegalArgumentException("La patente debe ser de 8 caracteres.");
+        }
+        
+        this.patente = patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnho() {
+        return anho;
+    }
+
+    public void setAnho(int anho) {
+        if (anho < 2000) {
+            throw new IllegalArgumentException("El año del vehiculo debe ser superior al 2000.");
+        }
+        
+        this.anho = anho;
+    }
+
+    public String getCondicion() {
+        return condicion;
+    }
+
+    public void setCondicion(String condicion) {
+        String condicion_upper = condicion.toUpperCase();
+        if (!"D".equals(condicion_upper) 
+                || !"A".equals(condicion_upper) 
+                || !"M".equals(condicion_upper)) {
+            throw new IllegalArgumentException("El tipo de condición no es valido.");
+        }
+        
+        this.condicion = condicion_upper;
+    }
+}
